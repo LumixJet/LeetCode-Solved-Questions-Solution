e2b314ed9d3cf93100aa6db89cf7c7cfeabd8956
Diwakar Tiwari <155289090+LumixJet@users.noreply.github.com>
2025-01-27 23:16:02 +0530

    Time: 353 ms (30.38%), Space: 234.1 MB (27.56%) - LeetHub

--- 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 3219-make-lexicographically-smallest-array-by-swapping-elements/3219-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,39 +1,36 @@
 class Solution {
 public:
-    vector<int> lexicographicallySmallestArray(vector<int>& arr, int threshold) {
-        vector<pair<int, int>> valueIndexPairs;
-        int size = arr.size();
+    vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) 
+    {
+        int n = nums.size();
+        vector<int> arr = nums; //to sort and find group of numbers
 
-        for (int i = 0; i < size; ++i) {
-            valueIndexPairs.push_back({arr[i], i});
+        int group = 0;
+        sort(arr.begin(),arr.end());
+
+        unordered_map<int,int> gNo;//to store group number of value
+        unordered_map<int, list<int>> gVal; //to store all val of each group in sorted form  
+        gNo[arr[0]] = group;
+        gVal[group].push_back(arr[0]);
+
+        for(int i=1;i<n;i++)
+        {
+            if(abs(arr[i] - arr[i-1]) > limit)
+            {                      //if adjacent element difference is greater than limit
+                group = group + 1; //that means that element is part of other group
+            }
+            gNo[arr[i]] = group;
+            gVal[group].push_back(arr[i]);
         }
 
-        sort(valueIndexPairs.begin(), valueIndexPairs.end());
-
-        vector<vector<pair<int, int>>> groupedPairs;
-        groupedPairs.push_back({valueIndexPairs[0]});
-
-        for (int i = 1; i < size; ++i) {
-            if (valueIndexPairs[i].first - valueIndexPairs[i - 1].first <= threshold) {
-                groupedPairs.back().push_back(valueIndexPairs[i]);
-            } else {
-                groupedPairs.push_back({valueIndexPairs[i]});
-            }
+        //finally building answer by placing each group smallest value at its group
+        vector<int> res(n); //current value which is present in first place of array 
+        for(int i=0;i<n;i++)
+        {
+            int gp = gNo[nums[i]]; //to know group number
+            res[i] = gVal[gp].front(); //get that group number in that place 
+            gVal[gp].pop_front(); //remove that value
         }
-
-        for (const auto& group : groupedPairs) {
-            vector<int> indices;
-            for (const auto& [value, index] : group) {
-                indices.push_back(index);
-            }
-
-            sort(indices.begin(), indices.end());
-
-            for (size_t i = 0; i < indices.size(); ++i) {
-                arr[indices[i]] = group[i].first;
-            }
-        }
-
-        return arr;
-    }
+        return res; //finally returning result which holds smallest lexographic order array
+    }              //which follows the given condition  |nums[i] - nums[j]| <= limit
 };
