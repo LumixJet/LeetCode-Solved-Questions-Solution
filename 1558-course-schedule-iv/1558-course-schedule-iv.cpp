464123afcd6527bf7ac235f9a5c78b03e80813a4
Diwakar Tiwari <155289090+LumixJet@users.noreply.github.com>
2025-01-30 23:07:04 +0530

    Time: 60 ms (52.22%), Space: 73 MB (39.33%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -1,27 +1,9 @@
 class Solution {
 public:
-    bool DFS(unordered_map<int, vector<int>> &adj, int u, int v, vector<bool> &visited)
-    {
-        visited[u] = true;
-        if(u == v)
-        {
-            return true; //reached from u --> v
-        }
-        for(auto &ngbr : adj[u])
-        {
-            if(!visited[ngbr])
-            {
-                if(DFS(adj,ngbr,v,visited))
-                {
-                    return true;
-                }
-            }
-        }
-        return false;
-    } 
-    vector<bool> checkIfPrerequisite(int nCourse, vector<vector<int>>& preReq, vector<vector<int>>& queries) 
+    vector<bool> checkIfPrerequisite(int nCourses, vector<vector<int>>& preReq, vector<vector<int>>& queries) 
     {
         unordered_map<int, vector<int>> adj;
+        vector<int> indegree(nCourses, 0); //vector to count indegree of vertex in graph 
 
         for(auto &edge : preReq)
         {
@@ -29,18 +11,54 @@ public:
             int v = edge[1];
 
             adj[u].push_back(v);
+            indegree[v] ++; //as edge is going from u to v (u-->v) so we increase indegree of v
         }
 
-        int n = queries.size();
-        vector<bool> result(n);
-        for(int i=0;i<n;i++) //checking for each query weather u is v prerequisute 
-        {                          //by calling DFS if yes then result is true else false
-            int u = queries[i][0];
-            int v = queries[i][1];
+        queue<int>q; //to store node which gets indegree 0 we store them to process its neighbour
+        for(int i=0;i<nCourses;i++)
+        {
+            if(indegree[i] == 0) //if indegree of vertex becomes 0 we push to queue
+            {
+                q.push(i);
+            }
+        }
 
-            vector<bool> visited(nCourse, false);
-            result[i] = DFS(adj,u,v,visited); //we can use any DFS BFS as we just need to check
-        }                                     //that weather from u node we can reach v node 
-        return result;
+        unordered_map<int, unordered_set<int>> mp; //to store pre-requisites of all vertex of graph
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+
+            for(auto &ngbr : adj[node]) //take neighbour of current node
+            {
+                mp[ngbr].insert(node); //as neighbour node pre-requisite will be current node
+
+                for(auto &preq : mp[node]) //check for pre-requisites of current nodes which are present in 
+                {                             //mp as that will too be pre-requisites of neighbour node
+                    mp[ngbr].insert(preq);
+                }
+                indegree[ngbr] --; //decrease neighbour node indegree by 1 as current node is processed 
+
+                if(indegree[ngbr] == 0) //if indegree of neighbour of node becomes 0 
+                {
+                    q.push(ngbr); //we push it to queue
+                }
+            }
+        }
+
+        int Q = queries.size();
+        vector<bool>  res(Q, false); //vector to store result of all queries 
+
+        for(int i=0;i<Q;i++) //traverse queries given to us 
+        {
+            int src = queries[i][0]; //find 1st node which will be source node 
+            int dest = queries[i][1]; //find 2nd node which will be destination node 
+
+            bool reach = mp[dest].contains(src); //if inside destination index of mp source node is 
+            res[i] = reach; //present that means source is pre-requisite of destination so we mark it true
+        }
+
+        return res;
+
     }
 };
