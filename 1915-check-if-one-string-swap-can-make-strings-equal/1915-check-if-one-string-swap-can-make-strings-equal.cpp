78fec32bbdc98e0cbf6b7728a49a5d90a4b88c34
Diwakar Tiwari <155289090+LumixJet@users.noreply.github.com>
2025-02-06 23:52:17 +0530

    Time: 0 ms (100%), Space: 8.3 MB (86.27%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
-    bool areAlmostEqual(string& s1, string& s2) {
-        const int n=s1.size();
-        int cntD=0, i;
-        array<char, 2> D={0};
-        for(i=0; i<n && cntD<=2; i++){
-            char x=s1[i], y=s2[i];
-            if (x!=y){
-                cntD++;
-                if (cntD>2 ||(cntD==2 && (D[0]!=y || D[1]!=x )))
-                    return 0;
-                D={x, y};
+    bool areAlmostEqual(string s1, string s2) 
+    {
+        set<int> temp1;
+        set<int> temp2;
+        int rev = 0;
+        int n = s1.length(); 
+        for(int i=0;i<n;i++)
+        {
+            if(s1[i] != s2[i])
+            {
+                if(temp2.find(s1[i]) != temp2.end() && temp1.find(s2[i]) != temp1.end())
+                {
+                    rev ++;
+                    temp1.erase(s2[i]);
+                    temp2.erase(s1[i]);
+                }
+                else
+                {
+                    temp1.insert(s1[i]);
+                    temp2.insert(s2[i]);
+                }
+
+                if(rev >= 2 || temp1.size() >= 2 && temp2.size() >=2)
+                {
+                    return false;
+                }
             }
         }
-        return cntD==0 || cntD==2;
+        if(temp1.empty() && temp2.empty())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 };
