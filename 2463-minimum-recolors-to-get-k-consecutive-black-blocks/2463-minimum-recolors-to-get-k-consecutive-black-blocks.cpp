074a4ab5509a13e0630a0c97588455c721a7fc84
Diwakar Tiwari <155289090+LumixJet@users.noreply.github.com>
2025-03-12 11:41:53 +0530

    Time: 3 ms (5.46%), Space: 8.3 MB (91.75%) - LeetHub

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    int minimumRecolors(string blocks, int k) {
-        int black_count = 0, ans = INT_MAX;
-        
-        for (int i = 0; i < blocks.size(); i++) {
-            if (i - k >= 0 && blocks[i - k] == 'B') black_count--;
-            if (blocks[i] == 'B') black_count++;
-            ans = min(ans, k - black_count);
-        }
-        
-        return ans;
+    int minimumRecolors(string blocks, int k) 
+    {
+        int mini = INT_MAX;
+        int nums = blocks.size();
+
+        for(int i=0; i<=nums-k; i++) //sliding window concept  
+        {                            //Number of times we need to iterate for an entire array as per our window 
+            int count = 0;
+            for(int j=i; j<i+k; j++) //each time we iterate to a window which is equal to size of k 
+            {
+                if(blocks[j] == 'W') //count number of W which is present in current window
+                {
+                    count ++;
+                }
+            }
+            mini = min(mini, count); //window having minimum number of W that much number of operation we require 
+        }                           //to Recolor K consecutive Black Blocks
+        return mini;
     }
 };
