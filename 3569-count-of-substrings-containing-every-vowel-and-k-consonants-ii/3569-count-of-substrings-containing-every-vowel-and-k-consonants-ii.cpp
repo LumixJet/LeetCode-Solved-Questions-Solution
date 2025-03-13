9d86e7d43316e337621793503fcc0ebad788fe49
Diwakar Tiwari <155289090+LumixJet@users.noreply.github.com>
2025-03-14 01:27:57 +0530

    Time: 147 ms (74.1%), Space: 57.6 MB (15.64%) - LeetHub

--- 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
+++ 3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -1,69 +1,92 @@
 class Solution {
 public:
-    typedef long long ll;
-    ll countOfSubstrings(string word, int k) {
+    bool isVowel(char ch) //function to check weather the character is vowel or consonant
+    {
+        return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+    }
+    long long countOfSubstrings(string word, int k) 
+    {
         int n = word.length();
-        int vowels[5] = {0};
-        int consonants = 0;
-        int i = 0;
-        int j = 0;
-        ll result = 0;
-        int leadig_Vowel = 0;
+        unordered_map<char,int> mpp; //to store vowel and there count in a perticular window
 
-        while(j < n){
-            // Count the character if it is vowel or consonants
-            if(word[j] == 'a') vowels[0]++;
-            else if(word[j] == 'e') vowels[1]++;
-            else if(word[j] == 'i') vowels[2]++;
-            else if(word[j] == 'o') vowels[3]++;
-            else if(word[j] == 'u') vowels[4]++;
-            else {
-                consonants++;
-            }
+        //Preprocessing to be able to find index of just next consonant 
 
-            // If window have more consonants than K Slide the window
-            while(consonants > k){
-                leadig_Vowel = 0;
-                if(word[i] == 'a') vowels[0]--;
-                else if(word[i] == 'e') vowels[1]--;
-                else if(word[i] == 'i') vowels[2]--;
-                else if(word[i] == 'o') vowels[3]--;
-                else if(word[i] == 'u') vowels[4]--;
-                else {
-                    consonants--;
-                }
-                i++;
-            }
+        vector<int> nxtConso(n); //to store index of next nearest consonant in string from current index
+        int lastConso = n; //variable to store nearest consonant as we start from last index of string so we initialize it with out of bound 
+                                                                        //index as consonant cant be present after last character in string
 
-            // If we found exactly K consonants
-            if(consonants == k){
-                bool flag = true;
-                // And all vowels exists atleast once
-                if(vowels[0] == 0 || vowels[1] == 0 || vowels[2] == 0 || vowels[3] == 0 || vowels[4] == 0){
-                    flag = false;
-                }
+        for(int i=n-1; i>=0; i--) //iterating from last of string to store nearest consonant of current character in string in O(1) time complexity
+        {
+            nxtConso[i] = lastConso; //store nearest consonant index at current character index of string
 
-                if(flag){
-                    // If there are vowels at start of window and they are extra ones means 
-                    // each vowel can make valid substring so we just count all leading extra vowels
-                    // and add it to results and add +1 also to count current valid Substring 
-                    while(consonants == k && i <= j){
-                        if(word[i] == 'a' && vowels[0] > 1) vowels[0]--;
-                        else if(word[i] == 'e' && vowels[1] > 1) vowels[1]--;
-                        else if(word[i] == 'i' && vowels[2] > 1) vowels[2]--;
-                        else if(word[i] == 'o' && vowels[3] > 1) vowels[3]--;
-                        else if(word[i] == 'u' && vowels[4] > 1) vowels[4]--;
-                        else {
-                            break;
-                        }
-                        i++;
-                        leadig_Vowel++;
-                    }
-                    result += leadig_Vowel+1;
-                }
-            }
-            j++;
+            if(!isVowel(word[i])) //if current character of string is not a vowel then initialize lastConso with current character 
+            {                    
+                lastConso = i; //index as this is nearest consonant from now onwards to all rest character of string present in left 
+            } 
         }
-        return result;  
+
+        //Now Normal Sliding Window Technique
+
+        int i=0; //used to shrink the window 
+        int j=0; //user to find a valid window
+
+        int conso = 0; //to store count of consonant for window
+        long long count = 0; //to store final answer which tells number of possible substrings which we can get
+
+        while(j<n)
+        {
+            char ch = word[j];
+            if(isVowel(ch)) //if current character is vowel then include it in map with its frequency 
+            {
+                mpp[ch] ++;
+            }
+            else //otheriwse increase the count of consonant
+            {
+                conso ++;
+            }
+
+            while(conso > k) //if window gets more than k consonant than it will not get valid string so we need 
+            {                          //we need to shrink the window size till we dont get consonant less than k
+
+                char ch = word[i]; 
+                if(isVowel(ch)) //if current character which is getting removed to shrink the window is vowel 
+                {
+                    mpp[ch] --; //we decrease its frequency in map
+                    if(mpp[ch] == 0) //if frequency of that vowel becomes 0 then we remove that character from map
+                    {
+                        mpp.erase(ch);
+                    }
+                }
+                else //otherwise if current character is consonant then we decrease count of consonant in window
+                {
+                    conso --;
+                }
+                i++; //and we keep on shrinking the size of window by incrementing i pointer till we dont get count of consonant less than k
+            }
+
+            while(mpp.size() == 5 && conso == k) //if we get valid window
+            {
+                int index = nxtConso[j]; //then we find nearest next consonant in right of current window from nxtConso vector
+                count += index - j; //and compute total number of valid strings which we can make  
+
+                char ch = word[i]; //after that we shrink the current window
+                if(isVowel(ch)) //if character which will get remove is vowel 
+                {
+                    mpp[ch] --; //then we decrease its frequency from map
+                    if(mpp[ch] == 0) //and if frequency of current character gets 0 than we remove current character from map
+                    {
+                        mpp.erase(ch);
+                    }
+                }
+                else //otherwise if current character is a consonant that we decrease count of consonant 
+                {
+                    conso --;
+                }
+                i++; //we keep on shrinking the size of window till the time we get valid window 
+            }
+            j++; //if we dont get valid window we keep on iterating the strind till the time we dont get valid window
+        }
+
+        return count; //at last we return total possible substring which we can get from given string  
     }
 };
