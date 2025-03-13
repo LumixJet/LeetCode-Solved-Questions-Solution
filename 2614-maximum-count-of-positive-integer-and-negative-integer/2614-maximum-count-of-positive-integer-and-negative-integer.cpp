967e967c09540faf597cebf51c295302e825256d
Diwakar Tiwari <155289090+LumixJet@users.noreply.github.com>
2025-03-14 01:29:20 +0530

    Time: 27 ms (0.37%), Space: 28.8 MB (8.35%) - LeetHub

--- 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/2614-maximum-count-of-positive-integer-and-negative-integer.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
-    int maximumCount(vector<int>& nums) {
-        int neg_count = binarySearch(nums, 0);
-        int pos_count = nums.size() - binarySearch(nums, 1);
-        return max(neg_count, pos_count);
-    }
-
-private:
-    int binarySearch(vector<int>& nums, int target) {
-        int left = 0, right = nums.size() - 1, result = nums.size();
-        
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] < target) {
-                left = mid + 1;
-            } else {
-                result = mid;
-                right = mid - 1;
+    int maximumCount(vector<int>& nums) 
+    {
+        int n = nums.size(); //use multiset as duplicate +ve or -ve numbers can be present
+        multiset<int> st1; //store -ve numbers present in nums
+        multiset<int> st2; //store +ve numbers present in nums
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i] < 0) //if current element of nums is less than 0 that means it is negative number
+            {
+                st1.insert(nums[i]); //we insert in st1
+            }
+            else if(nums[i] > 0) //if current element of nums is greater than 0 that means its a positive number
+            {
+                st2.insert(nums[i]); //we insert in st2
             }
         }
-        
-        return result;
+        if(st1.size() > st2.size()) //if size of st1 is greater than st2 that means negative number size is
+        {                                                             //greater than positive numbers in nums
+            return st1.size(); //so we return size of negative numbers
+        }
+        else //otherwise positive number is more than negative number 
+        {
+            return st2.size(); //therefore we return size of positive numbers present in nums
+        }    
     }
 };
