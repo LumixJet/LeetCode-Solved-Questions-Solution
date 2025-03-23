4989ece448a664b6e647148660d0a2a2b12a6203
Diwakar Tiwari <155289090+LumixJet@users.noreply.github.com>
2025-03-23 16:58:47 +0530

    Time: 91 ms (22.19%), Space: 139 MB (27.76%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.cpp
@@ -1,56 +1,53 @@
 class Solution {
 public:
-    int countCompleteComponents(int n, vector<vector<int>>& edges) {
-        vector<int> parent(n), rank(n, 0);
-        iota(parent.begin(), parent.end(), 0);
-        
-        function<int(int)> find = [&](int x) {
-            if (parent[x] != x) {
-                parent[x] = find(parent[x]);
-            }
-            return parent[x];
-        };
-        
-        auto unionSets = [&](int x, int y) {
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX == rootY) return;
-            if (rank[rootX] < rank[rootY]) {
-                parent[rootX] = rootY;
-            } else if (rank[rootX] > rank[rootY]) {
-                parent[rootY] = rootX;
-            } else {
-                parent[rootY] = rootX;
-                rank[rootX]++;
-            }
-        };
-        
-        for (auto& edge : edges) {
-            unionSets(edge[0], edge[1]);
-        }
-        
-        unordered_map<int, unordered_set<int>> componentVertices;
-        unordered_map<int, int> componentEdges;
-        
-        for (int i = 0; i < n; ++i) {
-            int root = find(i);
-            componentVertices[root].insert(i);
-        }
-        
-        for (auto& edge : edges) {
-            int root = find(edge[0]);
-            componentEdges[root]++;
-        }
-        
-        int completeCount = 0;
-        for (auto& [root, vertices] : componentVertices) {
-            int numVertices = vertices.size();
-            int expectedEdges = numVertices * (numVertices - 1) / 2;
-            if (componentEdges[root] == expectedEdges) {
-                completeCount++;
+    void dfs(int i, unordered_map<int,vector<int>> &adj, vector<bool> &vis, int &e, int &v)
+    {
+        vis[i] = true; //mark that node as true
+        v++; //count the vertex
+        e += adj[i].size(); //associated to that vertex count number of edges from adjacency list of graph
+
+        for(int ngbr : adj[i]) //now we find all neighbour of current vertex 
+        {
+            if(!vis[ngbr]) //if that neighbour vertex is not visited we call DFS over that neighbour vertex
+            {
+                dfs(ngbr,adj,vis,e,v);
             }
         }
-        
-        return completeCount;
+    }
+    int countCompleteComponents(int n, vector<vector<int>>& edges) 
+    {
+        unordered_map<int,vector<int>> adj; //forming a graph
+        int res = 0; //variable to store total number of connected component present 
+
+        for(auto edge : edges)
+        {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<bool> vis(n,false); //bool vector to mark which nodes are visited earlier
+
+        for(int i=0;i<n;i++) //traversing all the vertex present in graph
+        {
+            if(vis[i] == true) //if we get a vertex which is already visited then we continue to other vertex
+            {
+                continue;
+            }
+
+            int e = 0; //variable to store number of edges present 
+            int v = 0; //variable to store number of vertex present
+
+            dfs(i,adj,vis,e,v); //call DFS on the vertex which is not visited
+
+            if((v*(v-1) / 2) == e/2) //after counting number of vertex and edges present in component of graph
+            {                                               //we check the condition if it is getting satisfied
+
+                res ++; //means that component of graph is complete connected component of graph 
+            }
+        }
+        return res; //at last we return total number of connected component present in graph
     }
 };
