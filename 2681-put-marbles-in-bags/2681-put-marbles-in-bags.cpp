7c0b2cc7237b730bc9559de6c216610234c69fd6
Diwakar Tiwari <155289090+LumixJet@users.noreply.github.com>
2025-04-04 11:44:53 +0530

    Time: 31 ms (84.57%), Space: 64.3 MB (66.58%) - LeetHub

--- 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/2681-put-marbles-in-bags.cpp
@@ -1,20 +1,29 @@
 class Solution {
 public:
-    long long putMarbles(vector<int>& weights, int k) {
-        if (k == 1) {
-            return 0;
+    long long putMarbles(vector<int>& weights, int k) 
+    {
+        int n = weights.size(); //getting the size of weights array
+        int m = n-1; //size of pairSum array as pairs will have always -1 size of original array
+
+        vector<int> pairSum(m,0);
+        for(int i=0;i<m;i++)        //computing result for pair sum array 
+        {
+            pairSum[i] = weights[i] + weights[i+1]; //finding weights or sum of corresponding pairs
         }
 
-        vector<int> pair_sums;
-        for (size_t i = 0; i < weights.size() - 1; ++i) {
-            pair_sums.push_back(weights[i] + weights[i + 1]);
+        sort(pairSum.begin(),pairSum.end()); //sort the pair Sum array in ascending order
+
+        long long minSum = 0; //variable to find the sum of minimum pair 
+        long long maxSum = 0; //variable to find the sum of maximum pair
+
+        //As we have sorted the pairSum array in ascending order so to find answer for k bags we need to find min and max
+        //pair sum for k-1 pairs as that will give me correct answer 
+        for(int i=0;i<k-1;i++)
+        {
+            minSum += pairSum[i]; //min scores for k-1 pairs from pairSum array as in start pair will have min scores
+            maxSum += pairSum[m-i-1]; //max scores for k-1 pairs from pairSum array as in end pair will have max scores
         }
 
-        sort(pair_sums.begin(), pair_sums.end());
-
-        long long min_score = accumulate(pair_sums.begin(), pair_sums.begin() + (k - 1), 0LL);
-        long long max_score = accumulate(pair_sums.end() - (k - 1), pair_sums.end(), 0LL);
-
-        return max_score - min_score;
+        return maxSum - minSum; //At last we return ans as difference between max and min scores among marble distribution
     }
 };
