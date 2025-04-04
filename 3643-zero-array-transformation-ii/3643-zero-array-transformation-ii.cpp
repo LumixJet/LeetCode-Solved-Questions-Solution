b0c56f065d0687b6f18783eea69a7f6f0e1130a2
Diwakar Tiwari <155289090+LumixJet@users.noreply.github.com>
2025-04-04 12:33:21 +0530

    Time: 78 ms (14.78%), Space: 341.5 MB (26.18%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -1,26 +1,71 @@
 class Solution {
 public:
-    static int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        const int n=nums.size(), m=queries.size();
-        vector<int> freq(n+1);
-        int op=0, k=0;
-        for(int i=0; i<n; i++){
-            for( ; op < nums[i]-freq[i]; k++){
-                if (k>=m) return -1;
-                const int l=queries[k][0], r=queries[k][1], v=queries[k][2];
-                if (r<i) continue;
-                freq[max(l, i)]+=v;
-                freq[r+1]-=v;
-            }
-            op+=freq[i];
-        }
-        return k;
-    }
-};
+    int n; //for holding the size of nums  
+    int Q; //for holding the size of queries 
 
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();  
+    bool checkDiffArray(vector<int> &nums, vector<vector<int>> &queries, int k) //standard difference array technique code 
+    {
+        vector<int> diff(n,0);//create a difference array initialized all cells of array with 0
+
+        for(int i=0;i<k;i++) //process all queries till k length 
+        {
+            int l = queries[i][0]; //hold of start index of current query
+            int r = queries[i][1]; //hold of end index of current query
+            int val = queries[i][2]; //value which can be atmost subtracted and added from the range of indexes mentioned in queries
+
+                                                                                        //performing Difference array technique
+            diff[l] += val; //add val to start index mentioned in query in diff array 
+            if(r+1 < n) //if end+1 index of query is in bound 
+            {
+                diff[r+1] -= val; //subtract val from end+1 index mentioned in query from diff array
+            }
+        }
+        int cumSum = 0; //hold the cumilative sum of the diff array
+        for(int i=0;i<n;i++)
+        {
+            cumSum += diff[i]; //calculating cumilative sum from difference array till index i
+
+            diff[i] = cumSum; //hold cumilative sum in diff array at respective index 
+
+            if(nums[i] - diff[i] > 0) //as diff array says max change which can be made after processing k number of queries
+            {                        //if subtracting number present at index i from diff[i] does give value greater than 0 that 
+                return false; //means we cant form 0 in nums at that index so we return false that we need more queries
+            }
+        }
+        return true; //if all the indexes of nums we are able to form 0 with k number of queries we return true 
+    }
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) 
+    {
+        n = nums.size(); //storing the size of nums array 
+        Q = queries.size(); //storing the size of queries array
+
+        auto lambda = [](int x) 
+        {
+            return x == 0; //lambda function which return true when x is 0
+        };
+
+        if(all_of(nums.begin(),nums.end(),lambda)) //STL which sends all element present in nums to labda function
+        {
+            return 0; //if all elements present in nums are 0 then we return 0 as no operation is required to make nums as 0 array
+        }
+
+        int l = 0; //taking l and r to perform binary search on queries to optimally find minimum number of queries which are 
+        int r = Q-1;                                                                    //required to make nums as zero array
+        int k = -1; //to store final answer minimum number of queries which are required to make nums as zero array
+
+        while(l <= r) //performing binary search on queries
+        {
+            int mid = (l+r)/2; //computing mid for the query
+            if(checkDiffArray(nums,queries,mid+1)) //performing difference array technique till mid+1 queries and if it returns true 
+            {
+                k = mid+1; //then we mark mid+1 as answer and make r as mid-1 to find minimum number of queries whoich are
+                r = mid-1; //required to make nums array as zero array 
+            }   
+            else //if queries till mid+1 is not able to make nums array as zero array then we make l as mid+1 to increase  the 
+            {               //query range so that we can make nums array as zero array
+                l = mid+1;
+            }
+        }
+        return k; //at last we return k which has minimum number of queries which are required to make nums array as zero array 
+    }//if it is not possible to do so then k has -1 present in it which we return showing its not possible to make nums as zero array 
+};
